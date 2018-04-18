040b3130d51c3456bb620805bbd3c3d5e22a6cca
jake zenk <jake.zenk@ymail.com>
2018-04-18 02:57:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ def turn_count (board)
   num_turns = 0
   board.each do |token|
     if token == "X" || token == "O"
-      num_turns += 1
+      return num_turns += 1
     end
-  end  
+  end
 end
 
 def current_player (board)
